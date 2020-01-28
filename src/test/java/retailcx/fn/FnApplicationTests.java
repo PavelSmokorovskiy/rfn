1fe07e79ceb68674bd370c90d15544c4fd1b4a37
Pavel Smokorovskiy <pavel.smokorovskiy@pearlgroup.no>
2020-01-28 04:22:20 +0200

    Initial commit

---
+++ src/test/java/retailcx/fn/FnApplicationTests.java
@@ -0,0 +1,13 @@
+package retailcx.fn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
